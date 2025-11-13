f5526e8a233b1dec2170d7e4ce299ff021c574f6
XBotDoesStuff <Xanderchuck@gmail.com>
2025-11-13 11:51:33 -0700

    More stuff i dont remember

--- Scripts/weapon.gd
+++ Scripts/weapon.gd
@@ -4,18 +4,32 @@ extends Node2D
 @export var weapon_stats : WeaponStats:
 	set(value):
 		weapon_stats = value
-		call_deferred("update_visuals")
+		call_deferred("update_weapon")
+
+var proj_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_visuals()
-
+	update_weapon()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func update_visuals():
+func fire():
+	pass
+
+func update_weapon():
 	$AnimatedSprite2D.sprite_frames = weapon_stats.sprite
 	$AnimatedSprite2D.offset = weapon_stats.offset
+	
+	if weapon_stats.flip_h:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	if weapon_stats.flip_v:
+		$AnimatedSprite2D.flip_v = true
+	else:
+		$AnimatedSprite2D.flip_v =false
+		
 	$Muzzle.position = weapon_stats.muzzle_offset
--- Scripts/weaponStats.gd
+++ Scripts/weaponStats.gd
@@ -1,15 +1,21 @@
 class_name WeaponStats
 extends Resource
 
+@export_category("Positioning")
 @export var sprite : SpriteFrames
 @export var sprite_scale : Vector2
 @export var offset : Vector2
+@export var flip_v : bool
+@export var flip_h : bool
 @export var muzzle_offset : Vector2
 
+@export_category("Projectiles")
+@export var proj_scene : PackedScene
 @export var proj_count : int
 @export var proj_force : float
 @export var proj_range : float
 
+@export_category("Stats")
 @export var damage : float
 @export var pierce : int
 @export var fire_rate : float
